b7a3febea590b573fb48115c0520cdcf2c14606b
Max <Bruce.97@mail.ru>
2023-01-19 13:08:15 +0300

    task 29

---
+++ задача_29/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] randomArray (int length){
+
+    int [] array = new int [length];
+
+    for (int i=0; i < array.Length; i++){
+        array[i] = new Random().Next(-9, 10);
+    }
+
+    return array;
+}
+
+void showArray (int[] array){
+    for(int i=0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+}
+
+int [] array = randomArray(5);
+showArray(array);
