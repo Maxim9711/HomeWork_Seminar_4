2353cceb3283916778fc7d4b89ff9acae831b8fe
Max <Bruce.97@mail.ru>
2023-01-19 13:08:02 +0300

    task 27

---
+++ задача_27/Program.cs
@@ -0,0 +1,12 @@
+﻿int sumSymbol (int num){
+    int sumSymbol = 0;
+
+    while(Convert.ToBoolean(num)){
+        sumSymbol += num % 10;
+        num /= 10;
+    }
+
+    return sumSymbol;
+}
+
+Console.WriteLine(sumSymbol(77885555));
